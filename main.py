f8f993eb0ab43732b571e442b644a3b971be6b23
JoaoTorrinhas <jptorrinhas@gmail.com>
2023-06-06 18:21:59 +0100

    update main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask
-from AuthenticationAPI import create_app
+from AuthenticationAPI import *
+from flask_cors import CORS
 
 app = create_app()
-
+cors = CORS(app, resources={r"/*": {"origins": "http://webappfinder.deti"}})
+    
 if __name__ == '__main__':
+    #create_tables(app)
     app.run(debug=True, port=5001, host='0.0.0.0')
